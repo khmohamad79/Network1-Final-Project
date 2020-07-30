a04ea2ff3728980050a3ef49928b1004798d77b3
Mohamad <m.khodnama@gmail.com>
2020-07-30 20:04:57 +0430

    implement cast to str for http and dns.

--- Network1-Final-Project/Network1_Final_Project.py
+++ Network1-Final-Project/Network1_Final_Project.py
@@ -4,14 +4,49 @@ import transport_layer
 import application_layer
 
 import socket
-import struct 
+import struct
+import _thread
 
+def parse_packet(raw_data):
+    frame = link_layer.Ethernet(raw_data)
+    print(frame.src_mac)
+    print(frame.dest_mac)
+    print(type(frame))
+
+    segment = frame.next()
+    print(type(segment))
+    if(frame.proto==2054):
+        print("ARP Data:", str(segment.next()))
+    elif frame.proto==2048:
+        print("Source IP: ", segment.srcip)
+
+    datagram = segment.next()
+    print(type(datagram))
+
+    application = datagram.next()
+    print(type(application))
+
+
+def parse_packet2(raw_data):
+    layer = link_layer.Ethernet(raw_data)
+    print(type(layer))
+    while callable(getattr(layer, 'next', None)):
+        layer = layer.next()
+        print(type(layer))
+        if isinstance(layer, application_layer.HTTP):
+            print(layer)
+        if isinstance(layer, application_layer.DNS):
+            print(layer)
 
 conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
-
-while True:
+i=0
+while i<100:
+    i +=1
     raw_data, address = conn.recvfrom(65535)
-    segment = link_layer.Ethernet(raw_data)
+    parse_packet2(raw_data)
+    print(30*'-')
+    #_thread.start_new_thread(parse_packet2, (raw_data,))
+
 
 # main
 # frame = fetchFrame()
--- Network1-Final-Project/application_layer.py
+++ Network1-Final-Project/application_layer.py
@@ -43,6 +43,60 @@ class DNS:
 	def next(self):
 		return self.data[self.start:]
 
+	def __str__(self):
+		string = ""
+		string += "\tID : "+ str(self.ID) +"\n" 
+		string += "\tFlags: \n" +  "\t\tDNS "
+		if self.QR:
+			string += "Query\n"
+		else:
+			string += "Respone\n"
+		string += "\t\tOpcode: "
+		if self.opcode==0:
+			string += "Standard Query\n"
+		elif self.opcode==1:
+			string += "Inverse Query\n"
+		elif self.opcode==2:
+			string += "Server Status Request\n"
+		elif self.opcode==4:
+			string += "Notify\n"
+		elif self.opcode==5:
+			string += "Update\n"
+		else:
+			pass
+		string += "\t\tAuthorative: "
+		if self.AA:
+			string += "True\n"
+		else:
+			string += "False\n"
+		string += "\t\tTrunctuated: "
+		string += "True\n" if self.TC else "False\n"
+		string += "\t\tReccursion Desired: "
+		string += "True\n" if self.RD else "False\n"
+		string += "\t\tRecursion Available: "
+		string += "True\n" if self.RA else "False\n"
+		string += "\t\tAll Data is Authorative: "
+		string += "True\n" if self.AD else "False\n"
+		string += "\t\tChecking: "
+		string += "Disabled\n" if self.CD else "Enabled\n"
+		string += "\t\tR Code: " + str(self.Rcode) + "\n"
+		string += "\tQuestions: " + str(self.NoQues) + "\n"
+		string += "\tAnswers RR: " + str(self.NoAns) + "\n"
+		string += "\tAuthrative RR: " + str(self.NoAuth) + "\n"
+		string += "\tAdditional RR: " + str(self.NoAdd) + "\n"
+		string += "\tQueries: "
+		for i in self.Questions:
+			string += "\t\t" + str(i) + "\n"
+		for i in self.Answers:
+			string += "\t\t" + str(i) + "\n"
+		for i in self.Auth_ans:
+			string += "\t\t" + str(i) + "\n"
+		for i in self.Add_ans:
+			string += "\t\t" + str(i) + "\n"	
+
+		return string
+
+
 class HTTP:
 	def __init__(self, data: bytes):
 		self.data = data
@@ -67,8 +121,22 @@ class HTTP:
 			else:
 				mid = line.find(':')
 				self.headers[line[:mid]] = line[mid+2:-1]
-		for key in self.headers.keys():
-			print(key + ': ' + self.headers[key])
 
 	def next(self):
 		return self.nextlayer
+
+	def __str__(self):
+		output = 'HTTP; HyperText Transfer Protocol\n'
+		if hasattr(self, 'response_code'):
+			output += '\t' + self.response_version + ' ' + self.response_code + ' ' + self.response_phrase + '\n'
+			output += '\t\tResponse Version: ' + self.response_version + '\n'
+			output += '\t\tResponse Code: ' + self.response_code + '\n'
+			output += '\t\tResponse Phrase: ' + self.response_phrase + '\n'
+		else:
+			output += '\t' + self.request_method + ' ' + self.request_uri + ' ' + self.request_version + '\n'
+			output += '\t\tRequest Method: ' + self.request_method + '\n'
+			output += '\t\tRequest URI: ' + self.request_uri + '\n'
+			output += '\t\tRequest Version: ' + self.request_version + '\n'
+		for key in self.headers.keys():
+			output +=  '\t' + key + ': ' + self.headers[key] + '\n'
+		return output
