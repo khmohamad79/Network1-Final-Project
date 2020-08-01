81c1eeb9525dd3a1b3df2959d4c0efeaae14c2be
Mohamad <m.khodnama@gmail.com>
2020-08-02 00:27:19 +0430

    main thread.

--- Network1-Final-Project/Network1_Final_Project.py
+++ Network1-Final-Project/Network1_Final_Project.py
@@ -2,50 +2,36 @@ import link_layer
 import network_layer
 import transport_layer
 import application_layer
+from utilities import *
 
 import socket
 import struct
 import _thread
 
 def parse_packet(raw_data):
-    frame = link_layer.Ethernet(raw_data)
-    print(frame.src_mac)
-    print(frame.dest_mac)
-    print(type(frame))
-
-    segment = frame.next()
-    print(type(segment))
-    if(frame.proto==2054):
-        print("ARP Data:", str(segment.next()))
-    elif frame.proto==2048:
-        print("Source IP: ", segment.srcip)
-
-    datagram = segment.next()
-    print(type(datagram))
-
-    application = datagram.next()
-    print(type(application))
-
-
-def parse_packet2(raw_data):
     layer = link_layer.Ethernet(raw_data)
-    print(type(layer))
+    print(layer)
     while callable(getattr(layer, 'next', None)):
+        temp = type(layer).__name__
         layer = layer.next()
-        print(type(layer))
-        if isinstance(layer, application_layer.HTTP):
+        if isinstance(layer, bytes) and len(layer) > 0:
+            print('► ' + temp + ' Unparsed Data: ')
             print(layer)
-        if isinstance(layer, application_layer.DNS):
+            # print(bytes_to_ascii(layer))
+        elif isinstance(layer, bytes):
+            pass
+        else:
             print(layer)
 
 conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
 i=0
-while i<100:
+while i<500:
     i +=1
     raw_data, address = conn.recvfrom(65535)
-    parse_packet2(raw_data)
-    print(30*'-')
-    #_thread.start_new_thread(parse_packet2, (raw_data,))
+    parse_packet(raw_data)
+    print(80*'=')
+    print(80*'=')
+    #_thread.start_new_thread(parse_packet, (raw_data,))
 
 
 # main
